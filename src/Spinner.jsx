b3bfa4c977d9562a47ad888c8452dccf38b10b3f
Jaydeep <jaydeep.dey03@gmail.com>
2022-10-03 18:59:32 +0530

    added pokemon spinner

---
+++ src/Spinner.jsx
@@ -0,0 +1,11 @@
+import './Spinner.css'
+
+const Spinner = () => {
+    return (
+        <div>
+            <img className='loading-img' src="https://cdn-icons-png.flaticon.com/512/188/188987.png" alt="" />
+        </div>
+    )
+}
+
+export default Spinner
