78a2546506eb5601cdc2229317ab99900eacaee0
vaibhav-1508 <vaibhav.tripathi.mst21@iitbhu.ac.in>
2022-10-05 10:01:45 +0530

    Implemented lightmode/darkmode functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import PokeAPI from "./PokeAPI";
 import "./App.css";
+import { AppBar } from "@material-ui/core";
+import ButtonAppBar from "./Appbar";
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import Paper from "@material-ui/core/Paper";
+import { useState } from "react";
+import React from 'react';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createTheme({
+    palette: {
+      type: darkMode ? "dark" : "light"
+    }
+  })
   return (
     <>
-      <PokeAPI />
-    </>
+      <ThemeProvider theme={theme}>
+
+        <Paper style={{ height: "100%" }}>
+          <ButtonAppBar check={darkMode} change={() =>
+            setDarkMode(!darkMode)} />
+          <PokeAPI />
+        </Paper>
+
+      </ThemeProvider></>
+
   );
 }
 
---
+++ src/Appbar.js
@@ -0,0 +1,31 @@
+
+import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Switch from '@material-ui/core/Switch';
+import { Checkbox } from '@material-ui/core';
+import React from 'react';
+
+
+export default function ButtonAppBar({check, change}) {
+    const label = {
+        inputProps: { 'aria-label': 'Switch demo' }
+    };
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Dark Mode
+                    </Typography>
+                    <Switch {...label}
+                    checked = { check }
+                    onChange = { change }
+                    />
+
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
