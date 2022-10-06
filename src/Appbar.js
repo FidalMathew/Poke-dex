2294799b1ca7ed0ed628bd2e2ea4bf1e80c70f2f
vaibhav-1508 <vaibhav.tripathi.mst21@iitbhu.ac.in>
2022-10-06 09:32:34 +0530

    removed navbar

--- src/Appbar.js
+++ src/Appbar.js
@@ -7,24 +7,18 @@ import Switch from '@material-ui/core/Switch';
 import React from 'react';
 
 
-export default function ButtonAppBar({check, change}) {
+export default function ButtonAppBar({ check, change }) {
     const label = {
         inputProps: { 'aria-label': 'Switch demo' }
     };
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Dark Mode
-                    </Typography>
-                    <Switch {...label}
-                    checked = { check }
-                    onChange = { change }
-                    />
+        <>
+            <Switch {...label}
+                checked={check}
+                onChange={change}
+            />
+        </>
 
-                </Toolbar>
-            </AppBar>
-        </Box>
+
     );
 }
